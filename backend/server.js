58d8f17e63f152ab3ca4060026f2b935614f943f
Dinesh verma <vermadinesh9693@gmail.com>
2026-03-16 19:22:02 +0530

    commit for deployement

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.static(PUBLIC_DIR));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 // Middleware & routes
 app.use(authMiddleware);
